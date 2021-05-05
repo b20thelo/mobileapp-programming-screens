7b978b4152c30f3ac1b49be1ac6347836769c3af
Therese Lofstrom <b20thelo@student.his.se>
2021-05-05 16:06:43 +0200

    - Added image for ImageButton - Added ImageButton element in activity_main2.xml - Added code for ImageButton in MainActivity2.java - Added fragment and named FirstFragment.java - Added textelement in fragment_first.xml

--- app/src/main/java/com/example/screens/MainActivity2.java
+++ app/src/main/java/com/example/screens/MainActivity2.java
@@ -2,7 +2,13 @@ package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +16,15 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageButton buttonFragment = findViewById(R.id.accountButton);
+        buttonFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MainActivity2 -->", "Start fragment");
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new FirstFragment()).setReorderingAllowed(true).commit();
+            }
+        });
+
     }
 }
