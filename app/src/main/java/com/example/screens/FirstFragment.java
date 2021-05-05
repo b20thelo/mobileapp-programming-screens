dbd22837b0e527c5cb7bb2c9f5ff8a588770d8ef
Therese Lofstrom <b20thelo@student.his.se>
2021-05-05 21:43:00 +0200

    - Some design and size changes made.

--- app/src/main/java/com/example/screens/FirstFragment.java
+++ app/src/main/java/com/example/screens/FirstFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +60,18 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        ImageButton close = view.findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("First Fragment -->", "Close fragment");
+                getFragmentManager().beginTransaction().remove(getFragmentManager().findFragmentById(R.id.fragmentContainer)).setReorderingAllowed(true).commit();
+            }
+        });
+
+        return view;
     }
 }
